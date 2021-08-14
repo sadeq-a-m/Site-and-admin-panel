4e861ab990041129c36a52cf3d8ec8052c7026de
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2021-08-14 20:28:14 +0430

    set links

--- resources/views/home/sidbar.blade.php
+++ resources/views/home/sidbar.blade.php
@@ -13,14 +13,35 @@
                 <li><a href="#">همکاری</a></li>
                 <li><a href="#">درباره ما</a></li>
                 <li><a href="#">تماس با ما</a></li>
+                @if(Auth::check())
+                    <li><a href="{{route('panel.index')}}">پنل</a></li>
+
+                @endif
             </ul>
         </nav>
     </div>
+
+
+
+
+
     <div class="col-3 section-left">
         <div class="search"><a href="#"><i class="fas fa-search"></i></a></div>
         <div class="shop"><a href="#"><i class="fas fa-shopping-cart"></i></a></div>
-        <div class="login"><a href="#"><i class="fas fa-user"></i> ثبت نام | ورود</div>
+
+        @if(!Auth::check())
+
+        <div class="login"><a href="{{route('login')}}"><i class="fas fa-user"></i> ثبت نام | ورود</div>
+
+        @endif
     </div>
+
+
+
+
+
+
+
 </div>
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,11 +49,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('ورود') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('ثبت نام') }}</a>
                                 </li>
                             @endif
                         @else
